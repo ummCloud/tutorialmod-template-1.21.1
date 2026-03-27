5aeb2ad61c8339448ea0eea0c882a7735bc80f79
ummCloud <terry.nguyen06@gmail.com>
2026-03-26 20:11:28 -0500

    added something

--- src/main/java/net/umcloud/tutorialmod/TutorialmodClient.java
+++ src/main/java/net/umcloud/tutorialmod/TutorialmodClient.java
@@ -1,7 +1,7 @@
 package net.umcloud.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
-
+//hello
 public class TutorialmodClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
